a445cc1be8a544d368dc5f83ee0e0a1caefdf0cd
Dylan Perales <dperales@gmail.com>
2013-10-07 16:51:25 -0700

    Updated FilenameReplacer.py to accept user input within application rather than arguments. Works with Python 2.7 and up.

--- FilenameReplacer.py
+++ FilenameReplacer.py
@@ -2,25 +2,43 @@ import os
 import sys
 
 def rename_file( dir, src, dest ):
-    def rename_all( root, items ):
-        for name in items:
-            try:
-                if src in name: # REPLACE WITH SOURCE CHARACTERS
-                    print ("Found: {0}".format(name))
-                    os.rename(os.path.join(root, name), os.path.join(root, name.replace(src, dest, 1))) # REPLACE WITH BOTH SOURCE AND DESIRED CHARACTERS
-            except OSError as e:
-                print ("OSError ({0}): {1} - {2}".format(e.errno, e.strerror, name))
-    # starts from the bottom so paths further up remain valid after renaming
-    for root, dirs, files in os.walk( dir, topdown=False ):
-        rename_all(root,dirs)
-        rename_all(root,files)
-        
-if len(sys.argv) != 4: # Check for too many arguments
-    print ("Error: Invalid Arguments")
-    print ("Usage: FilenameReplacer.py \\DirToRename\\ srcChar destChar")
-else:
-    if os.path.isdir(sys.argv[1]): # Check for valid Path
-        rename_file(sys.argv[1], sys.argv[2], sys.argv[3])
-    else:
-        print ("Error: Invalid Path")
-        print ("Usage: FilenameReplacer.py \\DirToRename\\ srcChar destChar")
+	renameCount = 0
+	def rename_all( root, items ):
+		for name in items:
+			try:
+				if src in name: # REPLACE WITH SOURCE CHARACTERS
+					renameCount += 1
+					print ("Found: {0}".format(name))
+					os.rename(os.path.join(root, name), os.path.join(root, name.replace(src, dest, 1))) # REPLACE WITH BOTH SOURCE AND DESIRED CHARACTERS
+			except OSError as e:
+				print ("OSError ({0}): {1} - {2}".format(e.errno, e.strerror, name))
+	# starts from the bottom so paths further up remain valid after renaming
+	for root, dirs, files in os.walk( dir, topdown=False ):
+		rename_all(root,dirs)
+		rename_all(root,files)
+	if (renameCount == 0):
+		print ("Nothing found matching: " + src)
+		
+# Existing argument based checks
+#if len(sys.argv) != 4: # Check for too many arguments
+#    print ("Error: Invalid Arguments")
+#    print ("Usage: FilenameReplacer.py \\DirToRename\\ srcChar destChar")
+validDir = 0
+
+while (validDir == 0):
+	if (sys.version_info.major < 3):
+		renamePath = raw_input("Enter directory of contents you wish to rename: ")
+	else:
+		renamePath = input("Enter directory of contents you wish to rename: ")
+	if os.path.isdir(renamePath): # Check for valid Path
+		validDir = 1
+		if (sys.version_info.major < 3):
+			origName = raw_input("Enter name or string to replace: ")
+			newName = raw_input("Enter desired name or string: ")
+		else:
+			origName = input("Enter name or string to replace: ")
+			newName = input("Enter desired name or string: ")
+		rename_file(renamePath, origName, newName)
+	else:
+		print ("Error: Invalid Path")
+		print ("Example: C:\\DirToRename\\ or //home//user//folder//")
