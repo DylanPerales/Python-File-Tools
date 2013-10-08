0d9a7ee0f8e1c45c6819060734364887fdf2217e
Dylan Perales <dperales@gmail.com>
2013-10-07 22:32:49 -0700

    Updated FilenameReplacer.py to use user inputted variables within application, rather than using arguments. Application uses input or raw_input depending on python version being used.

--- FilenameReplacer.py
+++ FilenameReplacer.py
@@ -1,36 +1,39 @@
 import os
 import sys
+validDir = 0
+found = False
 
 def rename_file( dir, src, dest ):
-	renameCount = 0
 	def rename_all( root, items ):
 		for name in items:
 			try:
 				if src in name: # REPLACE WITH SOURCE CHARACTERS
-					renameCount += 1
-					print ("Found: {0}".format(name))
+					global found
+					found = True
+					newName = name.replace(src, dest, 1)
 					os.rename(os.path.join(root, name), os.path.join(root, name.replace(src, dest, 1))) # REPLACE WITH BOTH SOURCE AND DESIRED CHARACTERS
+					print ("Renamed: {0} to {1}".format(name, newName))
 			except OSError as e:
 				print ("OSError ({0}): {1} - {2}".format(e.errno, e.strerror, name))
 	# starts from the bottom so paths further up remain valid after renaming
 	for root, dirs, files in os.walk( dir, topdown=False ):
 		rename_all(root,dirs)
 		rename_all(root,files)
-	if (renameCount == 0):
-		print ("Nothing found matching: " + src)
 		
 # Existing argument based checks
 #if len(sys.argv) != 4: # Check for too many arguments
 #    print ("Error: Invalid Arguments")
 #    print ("Usage: FilenameReplacer.py \\DirToRename\\ srcChar destChar")
-validDir = 0
+
 
 while (validDir == 0):
 	if (sys.version_info.major < 3):
 		renamePath = raw_input("Enter directory of contents you wish to rename: ")
 	else:
 		renamePath = input("Enter directory of contents you wish to rename: ")
-	if os.path.isdir(renamePath): # Check for valid Path
+	if (len(renamePath) == 0):
+		exit(0)
+	elif os.path.isdir(renamePath): # Check for valid Path
 		validDir = 1
 		if (sys.version_info.major < 3):
 			origName = raw_input("Enter name or string to replace: ")
@@ -39,6 +42,8 @@ while (validDir == 0):
 			origName = input("Enter name or string to replace: ")
 			newName = input("Enter desired name or string: ")
 		rename_file(renamePath, origName, newName)
+		if (found == False):
+			print ("Nothing found matching: " + origName)
 	else:
 		print ("Error: Invalid Path")
 		print ("Example: C:\\DirToRename\\ or //home//user//folder//")
--- README.md
+++ README.md
@@ -24,17 +24,26 @@ Used to remove any capitalization (again recursively) within a folder.
 
 ### FilenameReplacer.py
 
-Used to modify multiple files filenames, replacing chars within filename (e.x. '-' to '_').
+Used to modify multiple files filenames, replacing strings within filename (e.x. '-' to '_').
 #### Usage:
-`FilenameReplacer.py \DirToRename\ srcChar destChar`
+`FilenameReplacer.py`
 
-Example: `FilenameReplacer.py \txtfiles\ - _`
+The program will then ask for the Directory to search, string to replace and what to replace it with
+Example: 
+'
+Enter directory of contents you wish to rename: E:\Test
+Enter name or string to replace: -
+Enter desired name or string: _
+Renamed: file-1.txt to file_1.txt
+Renamed: file-2.txt to file_2.txt
+Renamed: file-3.txt to file_3.txt
+'
 ##### Before:
-  * \txtfiles\file-1.txt 
-  * \txtfiles\file-2.txt 
-  * \txtfiles\file-3.txt 
+  * \test\file-1.txt 
+  * \test\file-2.txt 
+  * \test\file-3.txt 
 
 ##### After:
-  * \txtfiles\file_1.txt 
-  * \txtfiles\file_2.txt 
-  * \txtfiles\file_3.txt 
+  * \test\file_1.txt 
+  * \test\file_2.txt 
+  * \test\file_3.txt 
